e9ae403189d2691fb2aba3cfcd1f333527270302
Donata Deptula <dondeptu@gmail.com>
2025-04-06 23:50:34 +0200

    ButtonLink component added

---
+++ src/common/ButtonLink/index.js
@@ -0,0 +1,7 @@
+import { StyledNavLink } from "./styled";
+
+export const ButtonLink = ({ title }) => (
+    <StyledNavLink>
+        {title}
+    </StyledNavLink>
+);
---
+++ src/common/ButtonLink/styled.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+import { NavLink } from "react-router";
+
+export const StyledNavLink = styled(NavLink)`
+    text-decoration: none;
+    font-size: 18px;
+    color: currentColor;
+    border: 1px solid currentColor;
+    border-radius: 3px;
+    padding: 12px 16px;
+    box-shadow: 0 0 2px ${({ theme }) => theme.colors.textPrimary};
+    transition: background-color 0.3s ease;
+
+    &:hover {
+        background-color: ${({ theme }) => theme.colors.textPrimary};
+        color: ${({ theme }) => theme.colors.primary};
+        border-color: ${({ theme }) => theme.colors.textPrimary};
+    }
+`;
