ead41908f18b11858fbb0f28304612fe191aa775
Donata Deptula <dondeptu@gmail.com>
2025-04-11 02:33:40 +0200

    added section do WeddingHomepage

--- src/features/weddingHomepage/index.js
+++ src/features/weddingHomepage/index.js
@@ -1,7 +1,16 @@
 import { Header } from "./Header";
+import { Section } from "./Section";
+import ourPhoto from "./images/mountain-back-2.jpg";
+import sidePhoto from "./images/mountain-distant.jpg";
 
 export const WeddingHomepage = () => (
     <main>
         <Header />
+        <Section
+            title={"Przed nami najpiękniejsza przygoda życia – bądźcie z nami w tym wyjątkowym dniu!"}
+            text={"Ona szukała pomocy z programowaniem, on przyszedł z pomocą… i został na dłużej. Zaczęło się od zmiennych i funkcji, a skończyło na wspólnym życiu. Tak właśnie, z linijek kodu, powstała nasza historia – pełna śmiechu, czułości i miłości, która rośnie z każdym dniem (bez bugów!)."}
+            mainImgPath={ourPhoto}
+            sideImgPath={sidePhoto}
+        />
     </main>
 );
