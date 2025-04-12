141f7ea6183ed4ae554f797f756289af97a2e42f
Donata Deptula <dondeptu@gmail.com>
2025-04-12 09:35:53 +0200

    extract link to authorPath variable

--- src/common/Footer/index.js
+++ src/common/Footer/index.js
@@ -1,10 +1,14 @@
 import { FooterWrapper, Link, Paragraph } from "./styled";
 
-export const Footer = () => (
-    <FooterWrapper>
-        <Paragraph>
-            Design by&nbsp;
-            <Link target="_blank" rel="noreferrer noopener" href={"https://dondeptu.com/"}>Donata Deptuła</Link>
-        </Paragraph>
-    </FooterWrapper>
-);
+export const Footer = () => {
+    const authorPath = "https://dondeptu.com/";
+
+    return (
+        <FooterWrapper>
+            <Paragraph>
+                Design by&nbsp;
+                <Link target="_blank" rel="noreferrer noopener" href={authorPath}>Donata Deptuła</Link>
+            </Paragraph>
+        </FooterWrapper>
+    );
+}
